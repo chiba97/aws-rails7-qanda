11edc87d94fd4bb065b5d4089ca6ba53656a5a6a
chiba97 <ktfkyrya@gmail.com>
2022-11-03 03:38:28 +0000

    fix

--- config/puma.rb
+++ config/puma.rb
@@ -4,14 +4,17 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
-max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
-threads min_threads_count, max_threads_count
+# max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+# min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
+# threads min_threads_count, max_threads_count
+
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+threads threads_count, threads_count
 
 # Specifies the `worker_timeout` threshold that Puma will use to wait before
 # terminating a worker in development environments.
 #
-worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
+# worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
@@ -42,17 +45,17 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
 
-# bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
-# rails_root = Dir.pwd
-# # 本番環境のみデーモン起動
-# if Rails.env.production?
-#   pidfile File.join(rails_root, 'tmp', 'pids', 'puma.pid')
-#   state_path File.join(rails_root, 'tmp', 'pids', 'puma.state')
-#   stdout_redirect(
-#     File.join(rails_root, 'log', 'puma.log'),
-#     File.join(rails_root, 'log', 'puma-error.log'),
-#     true
-#   )
-#   # デーモン
-#   daemonize
-# end
+bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
+rails_root = Dir.pwd
+# 本番環境のみデーモン起動
+if Rails.env.production?
+  pidfile File.join(rails_root, 'tmp', 'pids', 'puma.pid')
+  state_path File.join(rails_root, 'tmp', 'pids', 'puma.state')
+  stdout_redirect(
+    File.join(rails_root, 'log', 'puma.log'),
+    File.join(rails_root, 'log', 'puma-error.log'),
+    true
+  )
+  # デーモン
+  daemonize
+end
